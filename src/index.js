0c8ab675f262cca7b4eeefb4f0baf7d43ea87573
Cristian Urueña <cristian.uruena26@gmail.com>
2023-01-28 18:46:42 -0500

    cors, middlewares and starting with frontend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const taskRoutes = require("./routes/task.routes.js");
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(taskRoutes);
+
+app.use((err, req, res, next) => {
+  return res.json({
+    message: err.message,
+  });
+});
 
 const port = 3000;
 
